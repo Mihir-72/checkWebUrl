5c6ae97c84fe01b95a2cdbc772a11b34b8a35751
Mihir-72 <144117994+Mihir-72@users.noreply.github.com>
2023-12-27 01:06:36 +0530

    Add files via upload

---
+++ checkWebUrl/script.js
@@ -0,0 +1,11 @@
+ // const currentUrl = window.location.href;
+
+    // console.log(currentUrl);
+
+    function res() {
+
+
+      const currentUrl = window.location.href;
+
+      document.getElementById("output").innerText = `Current URL:${currentUrl}`;
+    }
